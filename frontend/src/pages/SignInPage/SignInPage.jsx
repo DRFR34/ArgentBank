242831131429f7c7796dea0d440484bedc181063
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-07-23 22:54:53 +0200

    Create SignInPage.jsx

---
+++ frontend/src/pages/SignInPage/SignInPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import SignInForm from '../../components/SignInForm/SignInForm';
+import './SignInPage.scss';
+
+export default function SignInPage() {
+  
+
+  return (
+    <main className="main bg-dark">
+     
+        <SignInForm />
+      
+    </main>
+  );
+}
