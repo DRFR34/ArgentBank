3e8529fbf0d5ae13a772c5dfb08d7df3b0780424
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-07-23 23:16:42 +0200

    Update FeatureCard.jsx
    renaming classes

--- frontend/src/components/FeatureCard/FeatureCard.jsx
+++ frontend/src/components/FeatureCard/FeatureCard.jsx
@@ -8,23 +8,23 @@ import securityIcon from '../../assets/img/icon-security.png';
 
 export const cardOptions = {
 
-    opt1 : {
-        img : chatIcon,
-        alt : "Chat icon",
-        title : "You are our #1 priority",
-        text : "Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."
+    opt1: {
+        img: chatIcon,
+        alt: "Chat icon",
+        title: "You are our #1 priority",
+        text: "Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."
     },
-    opt2 : {
-        img : moneyIcon,
-        alt : "Money icon",
-        title : "More savings means higher rates",
-        text : "The more you save with us, the higher your interest rate will be!"
+    opt2: {
+        img: moneyIcon,
+        alt: "Money icon",
+        title: "More savings means higher rates",
+        text: "The more you save with us, the higher your interest rate will be!"
     },
-    opt3 : {
-        img : securityIcon,
-        alt : "Security icon",
-        title : "Security you can trust",
-        text : "We use top of the line encryption to make sure your data and money is always safe."
+    opt3: {
+        img: securityIcon,
+        alt: "Security icon",
+        title: "Security you can trust",
+        text: "We use top of the line encryption to make sure your data and money is always safe."
     }
 
 
@@ -32,11 +32,11 @@ export const cardOptions = {
 
 export default function FeatureCard(options) {
     return (
-        <div className="feature-item">
-            
-            <img src={options.img} alt={options.alt} className="feature-icon" />
-            <h3 className="feature-item-title">{options.title}</h3>
-            <p> {options.text} </p>
+        <div className="featureCard">
+
+            <img src={options.img} alt={options.alt} className="featureCard__icon" />
+            <h3 className="featureCard__title">{options.title}</h3>
+            <p className="featureCard__text" > {options.text} </p>
         </div>
     )
 }
