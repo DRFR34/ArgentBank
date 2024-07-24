f619bd9f4b50140fb7a251f666be2a6ec882665b
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-07-24 20:08:03 +0200

    Update NavBar.jsx
    Adding  JS-Docs and prop-Types

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,80 +1,87 @@
-import React, { useEffect } from 'react'
-import { NavLink, useLocation } from 'react-router-dom'
+import React, { useEffect } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
 import { logout } from '../../redux/features/auth/authSlice';
 import { fetchUserProfile } from '../../redux/features/user/userSlice';
+import logo from '../../assets/img/argentBankLogo.png';
+import './NavBar.scss';
 
-import { useDispatch, useSelector } from 'react-redux';
-import logo from '../../assets/img/argentBankLogo.png'
-import './NavBar.scss'
-
+/**
+ * Displays the navigation bar with sign-in and sign-out options.
+ * 
+ * @returns {JSX.Element} The NavBar component.
+ */
 export default function NavBar() {
     const authToken = useSelector((state) => state.auth.token);
     const storedToken = localStorage.getItem('token');
 
     let token = authToken || storedToken;
-    let tokenExist = token ? true : false;
+    let tokenExist = !!token;
 
     const location = useLocation();
-    const slug = location.pathname.split("/")[1]; 
+    const slug = location.pathname.split("/")[1];
     const dispatch = useDispatch();
 
     const firstName = useSelector((state) => state.user.firstName);
+    
     useEffect(() => {
-        dispatch(fetchUserProfile());
-    }, [dispatch, slug]);
+        if (tokenExist) {
+            dispatch(fetchUserProfile());
+        }
+    }, [dispatch, slug, tokenExist]);
 
     return (
         <header>
             <nav className="nav">
-
                 <NavLink
                     className='nav__link'
                     to="/">
                     <img className='nav__logo' src={logo} alt="Argent Bank Logo" />
                 </NavLink>
 
-                {!tokenExist
-                    ? (
+                {!tokenExist ? (
+                    <NavLink
+                        className={`nav__link ${(slug === "sign-in") && "hidden"}`}
+                        to="/sign-in">
+                        <div className='nav__link__signInBox'>
+                            <i className="fa fa-user-circle"></i>
+                            <span className='nav__span'> Sign In</span>
+                        </div>
+                    </NavLink>
+                ) : (
+                    <div className='nav__link__signOutBox'>
                         <NavLink
-                            className={`nav__link ${(slug === "sign-in") && "hidden"}`}
-                            to="/sign-in">
-                            <div className='nav__link__signInBox'>
+                            className='nav__link'
+                            to="/user"
+                            disabled={slug === "user"}
+                            style={slug === "user" ? { pointerEvents: "none", textDecoration: "none" } : {}}
+                        >
+                            <div className='nav__link__signOutBox__nameWrapper'>
                                 <i className="fa fa-user-circle"></i>
-                                <span className='nav__span'> Sign In</span>
+                                <span className='nav__span'> {firstName} </span>
                             </div>
                         </NavLink>
-                    )
-                    : (
 
-                        <div className='nav__link__signOutBox'>
-
-                            <NavLink
-                                className='nav__link'
-                                to="/user"
-                                disabled={slug === "user"}
-                                style={slug === "user"?({pointerEvents: "none", textDecoration: "none"}):({})}
-                            >
-                                <div className='nav__link__signOutBox__nameWrapper'>
-                                    <i className="fa fa-user-circle"></i>
-
-                                    <span className='nav__span'> {firstName} </span>
-                                </div>
-                            </NavLink>
-
-                            <NavLink
-                                className='nav__link'
-                                onClick={() => dispatch(logout())}
-                                to="/sign-in"
-                            >
-                                <i className="fa fa-sign-out"></i>
-
-                                <span className='nav__span'>Sign out</span>
-                            </NavLink>
-                        </div>
-
-                    )}
-
+                        <NavLink
+                            className='nav__link'
+                            onClick={() => dispatch(logout())}
+                            to="/sign-in"
+                        >
+                            <i className="fa fa-sign-out"></i>
+                            <span className='nav__span'>Sign out</span>
+                        </NavLink>
+                    </div>
+                )}
             </nav>
         </header>
-    )
+    );
 }
+
+NavBar.propTypes = {
+    authToken: PropTypes.string,
+    storedToken: PropTypes.string,
+    tokenExist: PropTypes.bool,
+    firstName: PropTypes.string,
+    slug: PropTypes.string,
+};
