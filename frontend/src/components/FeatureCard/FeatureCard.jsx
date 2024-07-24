3337670783239d274b9b53a103e44877740c7ef8
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-07-24 20:00:14 +0200

    Update FeatureCard.jsx
    Adding  JS-Docs and prop-Types

--- frontend/src/components/FeatureCard/FeatureCard.jsx
+++ frontend/src/components/FeatureCard/FeatureCard.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-
-import './FeatureCard.scss'
+import React from 'react';
+import PropTypes from 'prop-types';
+import './FeatureCard.scss';
 
 import chatIcon from '../../assets/img/icon-chat.png';
 import moneyIcon from '../../assets/img/icon-money.png';
 import securityIcon from '../../assets/img/icon-security.png';
 
+/**
+ * Options for the FeatureCard component.
+ * 
+ * @type {Object}
+ * @property {Object} optX, the Xth group of options for the feature card.
+ * @property {string} optX.img, The image source for the feature card.
+ * @property {string} optX.alt - The alt text for the image.
+ * @property {string} optX.title - The title of the feature card.
+ */
 export const cardOptions = {
-
     opt1: {
         img: chatIcon,
         alt: "Chat icon",
@@ -26,17 +34,31 @@ export const cardOptions = {
         title: "Security you can trust",
         text: "We use top of the line encryption to make sure your data and money is always safe."
     }
+};
 
-
-}
-
-export default function FeatureCard(options) {
+/**
+ * FeatureCard component displays a card with an icon, title, and a text.
+ * 
+ * @param {Object} props - Component properties.
+ * @param {string} props.img - The image source for the feature card.
+ * @param {string} props.alt - The alt text for the image.
+ * @param {string} props.title - The title of the feature card.
+ * @param {string} props.text - The text description of the feature card.
+ * @returns {JSX.Element} The FeatureCard component.
+ */
+export default function FeatureCard({ img, alt, title, text }) {
     return (
         <div className="featureCard">
-
-            <img src={options.img} alt={options.alt} className="featureCard__icon" />
-            <h3 className="featureCard__title">{options.title}</h3>
-            <p className="featureCard__text" > {options.text} </p>
+            <img src={img} alt={alt} className="featureCard__icon" />
+            <h3 className="featureCard__title">{title}</h3>
+            <p className="featureCard__text">{text}</p>
         </div>
-    )
+    );
 }
+
+FeatureCard.propTypes = {
+    img: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+};
