5e6d63a86e84a125fb7ca5d8dc126aa1e1d6f0f5
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-07-24 20:14:54 +0200

    Update SignInPage.jsx
    Adding  JS-Docs

--- frontend/src/pages/SignInPage/SignInPage.jsx
+++ frontend/src/pages/SignInPage/SignInPage.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import SignInForm from '../../components/SignInForm/SignInForm';
 import './SignInPage.scss';
 
+/**
+ * Renders the SignInPage component.
+ *
+ * @return {JSX.Element} The rendered SignInPage component.
+ */
 export default function SignInPage() {
   
 
