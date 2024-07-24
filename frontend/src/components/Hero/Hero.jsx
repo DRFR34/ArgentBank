86e74b916953880536ef463e04a355443dc929be
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-07-24 20:06:50 +0200

    Update Hero.jsx
    Adding  JS-Docs and prop-Types, and renaming classes with BEM syntax

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import React from 'react';
 
+import './Hero.scss';
+
+/**
+ * Displays a hero section with the claimed benefits of ArgentBank.
+ *
+ * @return {JSX.Element} The Hero component.
+ */
 export default function Hero() {
   return (
-    <div class="hero">
-        <section className="hero-content">
-          <h2 className="sr-only">Promoted Content</h2>
-          <p className="subtitle">No fees.</p>
-          <p className="subtitle">No minimum deposit.</p>
-          <p className="subtitle">High interest rates.</p>
-          <p className="text">Open a savings account with Argent Bank today!</p>
-        </section>
-      </div>
+    <div className="hero">
+
+      <section className="hero__content">
+
+        <p className="hero__content__subtitle">
+          No fees.
+        </p>
+
+        <p className="hero__content__subtitle">No minimum deposit.</p>
+
+        <p className="hero__content__subtitle">
+          High interest rates.
+        </p>
+
+        <p className="hero__content__text">
+          Open a savings account with Argent Bank today!
+        </p>
+
+      </section>
+
+    </div>
   )
 }
