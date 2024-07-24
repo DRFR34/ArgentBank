0f6e9a2e869b5b842f7163d1adbb9265b8d63416
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-07-24 19:54:36 +0200

    Update ChangeNameForm.jsx
    Adding  JS-Docs and prop-Types

--- frontend/src/components/ChangeNameForm/ChangeNameForm.jsx
+++ frontend/src/components/ChangeNameForm/ChangeNameForm.jsx
@@ -1,40 +1,50 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
-
-import './ChangeNameForm.scss'
+import './ChangeNameForm.scss';
 import { updateUserProfile } from '../../redux/features/user/userSlice';
+
+/**
+ * ChangeNameForm component allows the user to change their first and last name.
+ * 
+ * @returns {JSX.Element} The ChangeNameForm component.
+ */
 export default function ChangeNameForm() {
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
     const [newFirstname, setNewFirstname] = useState('');
     const [newLastname, setNewLastname] = useState('');
-    const [formOn, setformOn] = useState(false);
+    const [formOn, setFormOn] = useState(false);
     const [responseBoxOn, setResponseBoxOn] = useState(false);
     const updateResponseMsg = useSelector((state) => state.user.updateResponseMsg);
 
-
     useEffect(() => {
-
     }, [formOn, newFirstname, newLastname, updateResponseMsg]);
 
+    /**
+     * Handles the submission of the change name form.
+     * 
+     * @param {Object} e - The event object.
+     */
     const submitChangeNameForm = (e) => {
         e.preventDefault();
         dispatch(updateUserProfile({ firstName: newFirstname, lastName: newLastname }));
         setNewFirstname('');
         setNewLastname('');
-        setformOn(false);
+        setFormOn(false);
         setResponseBoxOn(true);
     };
 
-    const cancelAndHyde = (e) => {
+    /**
+     * Cancels the form and hides it.
+     */
+    const cancelAndHyde = () => {
         setNewFirstname('');
         setNewLastname('');
-        setformOn(false);
+        setFormOn(false);
     };
 
     return (
-        <div
-            className='changeNameWrapper '
-        >
+        <div className='changeNameWrapper'>
             <div className={`changeNameWrapper__responseBox ${responseBoxOn && "isVisible"}`}>
                 <h3>Result of your request :</h3>
                 <br />
@@ -42,32 +52,25 @@ export default function ChangeNameForm() {
                 <button
                     type="button"
                     className={`close-button `}
-                    onClick={() => {
-                        setResponseBoxOn(false);
-                    }}
-
-                >Close
+                    onClick={() => setResponseBoxOn(false)}
+                >
+                    Close
                 </button>
-
             </div>
 
             <button
                 type="button"
                 className={`edit-button ${(formOn || responseBoxOn) && "hidden"}`}
-                onClick={() => {
-                    setformOn(true);
-                }}
-
-            >Edit Name
+                onClick={() => setFormOn(true)}
+            >
+                Edit Name
             </button>
 
             <form
                 className={`changeNameForm ${formOn && "isVisible"}`}
                 onSubmit={submitChangeNameForm}
             >
-
-                <h2 className='changeNameForm__title' >Change your name data</h2>
-
+                <h2 className='changeNameForm__title'>Change your name data</h2>
                 <fieldset className='changeNameForm__fieldset'>
                     <label htmlFor="newFirstname">New first name</label>
                     <input
@@ -90,28 +93,27 @@ export default function ChangeNameForm() {
                         onChange={(e) => setNewLastname(e.target.value)}
                     />
                 </fieldset>
-
                 <div className='changeNameForm__buttonsBox'>
-
                     <button
                         type="submit"
                         className="changeNameForm__buttonsBox__submitBtn"
-                        disabled={(newFirstname === '' || newLastname === '') ? true : false}
+                        disabled={newFirstname === '' || newLastname === ''}
                     >
                         Save
                     </button>
-
                     <button
                         type="reset"
                         className="changeNameForm__buttonsBox__cancelBtn"
-                        onClick={() => { cancelAndHyde() }}
+                        onClick={cancelAndHyde}
                     >
                         Cancel
                     </button>
-
                 </div>
-
             </form>
         </div>
-    )
+    );
 }
+
+ChangeNameForm.propTypes = {
+    updateResponseMsg: PropTypes.string,
+};
