01db5c3cbafc28e177990ff73d09b9259dc2fe58
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-07-24 20:17:12 +0200

    Update TransactionsPage.jsx
    Adding  JS-Docs and prop-Types

--- frontend/src/pages/TransactionsPage/TransactionsPage.jsx
+++ frontend/src/pages/TransactionsPage/TransactionsPage.jsx
@@ -7,7 +7,13 @@ import './TransactionsPage.scss';
 import EditTransactionModal from '../../components/EditTransactionModal/EditTransactionModal';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import { setSearchValue, filterTransactions, setFilteredTransactionData } from '../../redux/features/transactionsSlice/transactionsSlice';
+import PropTypes from 'prop-types';
 
+/**
+ * Dysplays the user's transactions of an account, for the current month.
+ * 
+ * @returns {JSX.Element} The TransactionsPage component.
+ */
 export default function TransactionsPage() {
     const stokedToken = localStorage.getItem('token');
     const authToken = useSelector((state) => state.auth.token);
@@ -18,14 +24,27 @@ export default function TransactionsPage() {
     const transactionsData = useSelector((state) => state.transactions.transactionsData);
     const selectedTransaction = useSelector((state) => state.transactions.selectedTransaction);
 
+    /**
+     * Handles the edit button click for a transaction.
+     * 
+     * @param {Object} transaction - The transaction object to be edited.
+     */
     const handleEditClick = (transaction) => {
         dispatch({ type: 'transactions/setSelectedTransaction', payload: transaction });
     };
 
+    /**
+     * Handles closing the edit transaction modal.
+     */
     const handleCloseModal = () => {
         dispatch({ type: 'transactions/setSelectedTransaction', payload: null });
     };
 
+    /**
+     * Handles changing the search value.
+     * 
+     * @param {string} value - The new search value.
+     */
     const handleSearchChange = (value) => {
         dispatch(setSearchValue(value));
         if (value.length >= 3) {
@@ -35,6 +54,9 @@ export default function TransactionsPage() {
         }
     };
 
+    /**
+     * Handles clicking the search button.
+     */
     const handleSearchClick = () => {
         dispatch(filterTransactions());
     };
@@ -74,3 +96,16 @@ export default function TransactionsPage() {
         </main>
     );
 }
+
+TransactionsPage.propTypes = {
+    stokedToken: PropTypes.string,
+    authToken: PropTypes.string,
+    token: PropTypes.string,
+    searchValue: PropTypes.string,
+    transactionsData: PropTypes.arrayOf(PropTypes.object),
+    selectedTransaction: PropTypes.object,
+    handleEditClick: PropTypes.func,
+    handleCloseModal: PropTypes.func,
+    handleSearchChange: PropTypes.func,
+    handleSearchClick: PropTypes.func,
+};
