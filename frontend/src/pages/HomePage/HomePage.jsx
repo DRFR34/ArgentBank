32d4a8d9098c24e50c5708300d4d61404bbc3fb2
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-07-24 20:14:20 +0200

    Update HomePage.jsx
    Adding  JS-Docs

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,23 +1,20 @@
 import React from 'react'
 
+
+import Hero from '../../components/Hero/Hero'
 import FeatureCard, { cardOptions } from '../../components/FeatureCard/FeatureCard'
 
 import './HomePage.scss'
+
+/**
+ * Displays the home page with hero section and feature cards.
+ * 
+ * @returns {JSX.Element} The HomePage component.
+ */
 export default function HomePage() {
   return (
     <main>
-      <section className="hero">
-        <div className="hero-content">
-          <h2 className="sr-only">Promoted Content</h2>
-          <div className='hero-content__subtiltles'>
-          <p className="subtitle">No fees.</p>
-          <p className="subtitle">No minimum deposit.</p>
-          <p className="subtitle">High interest rates.</p>
-          </div>
-          <p className="text">Open a savings account with Argent Bank today!</p>
-        </div>
-
-      </section> 
+      <Hero />
         
         < section className='featuresGrid'>
 
