573a9566704a1a079d0b7ce72860e2ff747cfddd
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-07-24 13:19:32 +0200

    Update NavBar.jsx

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,27 +1,80 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { NavLink, useLocation } from 'react-router-dom'
+import { logout } from '../../redux/features/auth/authSlice';
+import { fetchUserProfile } from '../../redux/features/user/userSlice';
 
+import { useDispatch, useSelector } from 'react-redux';
 import logo from '../../assets/img/argentBankLogo.png'
-
 import './NavBar.scss'
 
 export default function NavBar() {
-  return (
-    <header>
-      <nav className="nav">
+    const authToken = useSelector((state) => state.auth.token);
+    const storedToken = localStorage.getItem('token');
 
-         <NavLink to="/">
-          <img className='nav__logo' src={logo} alt="Argent Bank Logo" />
-        </NavLink>
+    let token = authToken || storedToken;
+    let tokenExist = token ? true : false;
 
-       <NavLink to="/sign-in">
-          <div className='SignInBox'>
-            <i className="fa fa-user-circle"></i>
-            <span>Sign In</span>
-          </div>
-        </NavLink>
+    const location = useLocation();
+    const slug = location.pathname.split("/")[1]; 
+    const dispatch = useDispatch();
 
-      </nav>
-    </header>
-  )
+    const firstName = useSelector((state) => state.user.firstName);
+    useEffect(() => {
+        dispatch(fetchUserProfile());
+    }, [dispatch, slug]);
+
+    return (
+        <header>
+            <nav className="nav">
+
+                <NavLink
+                    className='nav__link'
+                    to="/">
+                    <img className='nav__logo' src={logo} alt="Argent Bank Logo" />
+                </NavLink>
+
+                {!tokenExist
+                    ? (
+                        <NavLink
+                            className={`nav__link ${(slug === "sign-in") && "hidden"}`}
+                            to="/sign-in">
+                            <div className='nav__link__signInBox'>
+                                <i className="fa fa-user-circle"></i>
+                                <span className='nav__span'> Sign In</span>
+                            </div>
+                        </NavLink>
+                    )
+                    : (
+
+                        <div className='nav__link__signOutBox'>
+
+                            <NavLink
+                                className='nav__link'
+                                to="/user"
+                                disabled={slug === "user"}
+                                style={slug === "user"?({pointerEvents: "none", textDecoration: "none"}):({})}
+                            >
+                                <div className='nav__link__signOutBox__nameWrapper'>
+                                    <i className="fa fa-user-circle"></i>
+
+                                    <span className='nav__span'> {firstName} </span>
+                                </div>
+                            </NavLink>
+
+                            <NavLink
+                                className='nav__link'
+                                onClick={() => dispatch(logout())}
+                                to="/sign-in"
+                            >
+                                <i className="fa fa-sign-out"></i>
+
+                                <span className='nav__span'>Sign out</span>
+                            </NavLink>
+                        </div>
+
+                    )}
+
+            </nav>
+        </header>
+    )
 }
