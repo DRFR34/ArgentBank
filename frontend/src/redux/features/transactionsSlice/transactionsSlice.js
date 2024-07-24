a96f15e3022e4702be65c0eb4e1a37ac5306c199
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-07-24 20:22:29 +0200

    Update transactionsSlice.js
    Adding  JS-Docs

--- frontend/src/redux/features/transactionsSlice/transactionsSlice.js
+++ frontend/src/redux/features/transactionsSlice/transactionsSlice.js
@@ -1,6 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit';
 import sampleData from '../../../assets/data/accountTableSampleData.json';
 
+/**
+ * A slice for managing transactions state.
+ * 
+ * @type {Slice}
+ * @param {string} name - The name of the slice.
+ * @param {Object} initialState - The initial state of the slice.
+ * @param {Array} initialState.transactionsData - The list of all transactions.
+ * @param {Array} initialState.filteredTransactionData - The list of filtered transactions based on search value.
+ * @param {string} initialState.searchValue - The current search value.
+ * @param {Object||null} initialState.selectedTransaction - The currently selected transaction.
+ * @param {Object} reducers - The reducer functions.
+ */
 const transactionsSlice = createSlice({
     name: 'transactions',
     initialState: {
@@ -10,16 +22,54 @@ const transactionsSlice = createSlice({
         selectedTransaction: null,
     },
     reducers: {
+        
+        /**
+         * Sets the transactions data.
+         * 
+         * Action type: 'transactions/setTransactionsData'
+         * 
+         * @param {Object} state - The current state.
+         * @param {Object} action - The action object.
+         * @param {Array} action.payload - The new transactions data.
+         */
         setTransactionsData: (state, action) => {
             state.transactionsData = action.payload;
             state.filteredTransactionData = action.payload;
         },
+
+        /**
+         * Sets the filtered transactions data.
+         * 
+         * Action type: 'transactions/setFilteredTransactionData'
+         * 
+         * @param {Object} state - The current state.
+         * @param {Object} action - The action object.
+         * @param {Array} action.payload - The new filtered transactions data.
+         */
         setFilteredTransactionData: (state, action) => {
             state.filteredTransactionData = action.payload;
         },
+
+        /**
+         * Sets the search value.
+         * 
+         * Action type: 'transactions/setSearchValue'
+         * 
+         * @param {Object} state - The current state.
+         * @param {Object} action - The action object.
+         * @param {string} action.payload - The new search value.
+         */
         setSearchValue: (state, action) => {
             state.searchValue = action.payload;
         },
+
+        /**
+         * Filters the transactions, based on the search value.
+         * 
+         * Action type: 'transactions/filterTransactions'
+         * 
+         * @param {Object} state - The current state.
+         */
         filterTransactions: (state) => {
             state.filteredTransactionData = state.transactionsData.filter((transaction) =>
                 Object.entries(transaction)
@@ -29,6 +79,16 @@ const transactionsSlice = createSlice({
                     )
             );
         },
+
+        /**
+         * Sets the selected transaction.
+         * 
+         * Action type: 'transactions/setSelectedTransaction'
+         * 
+         * @param {Object} state - The current state.
+         * @param {Object} action - The action object.
+         * @param {Object|null} action.payload - The selected transaction.
+         */
         setSelectedTransaction: (state, action) => {
             state.selectedTransaction = action.payload;
         },
