a6ba65b9d325d1261c6a97f7456a1ff1b4269c77
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-07-24 20:04:40 +0200

    Update Footer.jsx
    Adding  JS-Docs and prop-Types

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 import './Footer.scss'
+/**
+ * A simple Footer, displaying the current year in the copyright.
+ *
+ * @return {JSX.Element} The footer component.
+ */
 export default function Footer() {
 const currentYear = new Date().getFullYear()
 
